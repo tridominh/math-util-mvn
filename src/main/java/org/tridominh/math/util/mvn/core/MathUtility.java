0bb56c7f96d02d0692822a85c64efb7a747ea3c2
tridominh <tridmse173029@fpt.edu.vn>
2023-06-26 16:50:44 +0700

    update code to use recursion and see green4ever

--- src/main/java/org/tridominh/math/util/mvn/core/MathUtility.java
+++ src/main/java/org/tridominh/math/util/mvn/core/MathUtility.java
@@ -25,23 +25,23 @@ public class MathUtility {
     //    21 giai thừa kiểu long chứa ko đc
     //    hàm chỉ cho phép n = 0..20
     //    đưa n > 20; ném ra exception
-    public static long getF(int n){
-        if(n < 0 || n > 20) 
-            throw new IllegalArgumentException("Invalid n. n must be between 0..20, plz");
-        
-        if(n==0 || n==1) return 1;
-        
-        long product = 1;
-        for(int i = n; i >= 2; i--)
-            product *= i;
-        return product;
-    }
+    
+//    public static long getFactorial(int n){
+//        if(n < 0 || n > 20) 
+//            throw new IllegalArgumentException("Invalid n. n must be between 0..20, plz");
+//        
+//        if(n==0 || n==1) return 1;
+//        
+//        long product = 1;
+//        for(int i = n; i >= 2; i--)
+//            product *= i;
+//        return product;
+//    }
     
     public static long getFactorial(int n){
         if(n < 0 || n > 20) 
             throw new IllegalArgumentException("Invalid n. n must be between 0..20, plz");
         if(n == 0 || n == 1) return 1;
-        if(n > 1) return n * getFactorial(n-1);
-        return 0;
+        return n * getFactorial(n-1);
     }
 }
